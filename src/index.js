eb0ca682c050f4101fd160d405d70ea7126a5599
julianvasquez <julian.vasquez@netuxtecnologia.com>
2017-01-16 15:39:56 -0500

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-function createRT() {
+function createRT(actions) {
   return ({ dispatch, getState }) => next => (action) => {
     const state = getState()
-    const actions = [
-      { type: 'redux-form/BLUR' },
-      { type: 'redux-form/CHANGE', fields: ['aph1Id', 'aph2Id'] },
-      { type: 'SHOW_CASOPOSITIVO' },
-      { type: 'SHOW_CASOPOSITIVO' },
-      { type: 'ADD_COMMET' },
-      { type: 'REGISTRAR_HORA' },
-      { type: 'UPDATE_AMBDESPACHADA' },
-    ]
 
     if (typeof action === 'function') {
       return action(dispatch, getState)
