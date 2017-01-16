4f37baf2e8c6ac2baa54c67df33ff545bccf577e
julianvasquez <julian.vasquez@netuxtecnologia.com>
2017-01-16 12:24:44 -0500

    add static actions to test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 function createRT() {
-  return store => next => (action) => {
-    const state = store.getState()
+  return ({ dispatch, getState }) => next => (action) => {
+    const state = getState()
+    const actions = [
+      { type: 'redux-form/BLUR' },
+      { type: 'redux-form/CHANGE', fields: ['aph1Id', 'aph2Id'] },
+      { type: 'SHOW_CASOPOSITIVO' },
+      { type: 'SHOW_CASOPOSITIVO' },
+      { type: 'ADD_COMMET' },
+      { type: 'REGISTRAR_HORA' },
+      { type: 'UPDATE_AMBDESPACHADA' },
+    ]
+
+    if (typeof action === 'function') {
+      return action(dispatch, getState)
+    }
 
     if (state.global.socket) {
-      state.global.socket.emit('send', action.data)
+      const { meta, payload, type } = action
+      actions.map((a) => {
+        if (!a.fields && a.type === type) {
+          state.global.socket.emit('send', { meta, payload })
+        } else if (!a.fields.indexOf(meta.field) === -1) {
+          state.global.socket.emit('send', { meta, payload })
+        }
+        return a
+      })
     }
     return next(action)
   }
